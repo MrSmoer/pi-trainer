8b57402dcf39e029baf4f0d49a122ffb8d1f08ca
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2022-04-13 12:49:45 +0200

    moved content to correct py-file

--- pi-game.py
+++ pi-game.py
@@ -1,47 +1,109 @@
-#! /usr/bin/python3
-
-import sys
-from time import sleep
-from consoledraw import Console
-from datetime import datetime
-from pynput import keyboard
 import curses
-format = """
-    ╔══════════╗
-    ║ {} ║
-    ╚══════════╝
-"""
+from curses import wrapper
+from curses.ascii import isdigit
+import time
 
-def on_press(key):
-    try:
-        #print('alphanumeric key {0} pressed'.format(key.char))
-        with console:
-            console.print(format.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+lines = []
 
-def on_release(key):
-    #print('{0} released'.format(key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
- 
+keys='''╔═══╦═══╦═══╗
+║ 7 ║ 8 ║ 9 ║
+╠═══╬═══╬═══╣
+║ 4 ║ 5 ║ 6 ║
+╠═══╬═══╬═══╣
+║ 1 ║ 2 ║ 3 ║
+╠═══╩═══╬═══╝
+║  0    ║
+╚═══════╝'''
+keyCords=[(7,3),(5,2),(5,6),(5,10),(3,2),(3,6),(3,10),(1,2),(1,6),(1,10)]
+def exit(stdscr):
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+
+
+
 def has_numbers(inputString):
     return any(char.isdigit() for char in inputString)
 
-if __name__ == '__main__':
- 
-    #startDigit=input("From which digit do you want to start?:\n")
-    #print("start digit is: " + startDigit)
-    console = Console()
-    s = 'testfas'
-    with keyboard.Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
+def changeColorOfKey(keypad,char,color):
+    if char != '':
+        keypad.addch(keyCords[int(char)][0],keyCords[int(char)][1],char,color)
+        keypad.refresh()
 
-    print("Quitting")
+
+def main(stdscr):
+
+    if curses.has_colors:
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_GREEN)
+        curses.init_pair(0, -1, -1)
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
+        rightColor=curses.color_pair(1)
+        offColor=curses.color_pair(0)
+        wrongColor=curses.color_pair(2)
+    else:
+        rightColor=curses.A_STANDOUT
+        offColor=curses.A_NORMAL
+        wrongColor=curses.A_BLINK
+
+    pifile = open('pi.txt')
+    
+    while True:
+        # Get next line from file
+        line = pifile.readline()
+    # if line is empty
+    # end of file is reached
+        if not line:
+            break
+        if has_numbers(line) and "-" not in line and " " in line:
+            cleanedLine = ''
+            for c in line:
+                if c != ' ' and c.isdigit():
+                    cleanedLine = cleanedLine+c
+            #print("Line{}: {}".format(count, line.strip()))
+            lines.append(cleanedLine)
+    pifile.close()
+
+    # stops displaying keystrokes
+    curses.noecho()
+    # doesn't need return to get keys
+    curses.cbreak()
+    # disable cursor
+    curses.curs_set(0)
+    stdscr.keypad(True)
+    window = curses.newwin(curses.LINES-1, curses.COLS-1)
+    window.nodelay(False)
+    window.addstr(1, 5, lines[0])
+    window.refresh()
+    correctDigits=0
+    currentDigit=lines[0][0]
+    cL=0
+    keypad=curses.newwin(9,14,5,15)
+    keypad.addstr(keys)
+    keypad.refresh()
+    oldKey=""
+    while True:
+        a = window.getkey()
+        changeColorOfKey(keypad,oldKey,offColor)
+        oldKey=a
+        if a.isdigit():
+            if a==currentDigit:
+                window.addstr(1, 5, lines[cL][correctDigits+1:]+lines[cL+1][0:correctDigits])
+                window.refresh()
+                correctDigits+=1
+                currentDigit=lines[cL][correctDigits]
+                changeColorOfKey(keypad,a,rightColor)
+                
+            else:
+                print('\a', end="",flush=True)
+                changeColorOfKey(keypad,a,wrongColor)
+            
+        elif a == 'q':
+            break
+    exit(stdscr)
+
+wrapper(main)
         
         
 
