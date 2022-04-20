3183b11cd20ba20b716ad061858aef7e561bdac8
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2022-04-20 12:43:28 +0200

    properly handling file closing

--- piTrainer/PiFileReader.py
+++ piTrainer/PiFileReader.py
@@ -6,22 +6,22 @@ class PiFileReader:
         return any(char.isdigit() for char in inputString)
 
     def readPifileToLines(self,filename):
-        pifile = open(filename)
-        lines=[]
-        
-        while True:
-            # Get next line from file
-            line = pifile.readline()
-        # if line is empty
-        # end of file is reached
-            if not line:
-                break
-            if self.has_numbers(line) and "-" not in line and " " in line:
-                cleanedLine = ''
-                for c in line:
-                    if c != ' ' and c.isdigit():
-                        cleanedLine = cleanedLine+c
-                #print("Line{}: {}".format(count, line.strip()))
-                lines.append(cleanedLine)
-        pifile.close()
+        with open(filename) as pifile:
+            lines=[]
+            
+            while True:
+                # Get next line from file
+                line = pifile.readline()
+            # if line is empty
+            # end of file is reached
+                if not line:
+                    break
+                if self.has_numbers(line) and "-" not in line and " " in line:
+                    cleanedLine = ''
+                    for c in line:
+                        if c != ' ' and c.isdigit():
+                            cleanedLine = cleanedLine+c
+                    #print("Line{}: {}".format(count, line.strip()))
+                    lines.append(cleanedLine)
+
         return lines
